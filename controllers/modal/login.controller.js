1d8854b3852168ecce397a55b892020cb0304cfb
Ambar <ambar.bhatnagar@sap.com>
2019-06-11 01:58:24 +0530

    New html changes.

--- controllers/modal/login.controller.js
+++ controllers/modal/login.controller.js
@@ -5,8 +5,8 @@
         .module('app')
         .controller('LoginController', LoginController);
 
-    LoginController.$inject = ['$rootScope', 'QueryService', '$scope', 'API', 'ngTableParams', 'toaster', 'close', '$http'];
-    function LoginController($rootScope, QueryService, $scope, API, ngTableParams, toaster, close, $http) {
+    LoginController.$inject = ['$rootScope', 'QueryService', '$scope', 'API', 'ngTableParams', 'toaster', 'close', '$http', '$httpParamSerializer'];
+    function LoginController($rootScope, QueryService, $scope, API, ngTableParams, toaster, close, $http, $httpParamSerializer) {
 
         console.log('login');
         $scope.close = close;
@@ -18,6 +18,7 @@
 
         $scope.submitForm = function(){
             console.log('submit');
+
             let login_details = {
                 username: $scope.login.username,
                 password: $scope.login.password,
@@ -25,28 +26,22 @@
                 userType: '1'
             }
 
-            $http.post(API.BaseUrl+'login', login_details,{
+            $http({
+                url: API.BaseUrl+'login',
+                method: 'POST',
+                data: $httpParamSerializer(login_details), // Make sure to inject the service you choose to the controller
                 headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded',
-                    'Authorization': 'Basic VFY6TFVJU1RWQDEyMw=='
+                  'Content-Type': 'application/x-www-form-urlencoded', // Note the appropriate header
+                  'Authorization': 'Basic VFY6TFVJU1RWQDEyMw==' 
                 }
-            })
+              })
 
             .then(function(res){
                 console.log('then', res);
+                window.localStorage.setItem('accessToken', res.data.data.accessToken);
             }).catch(function(res){
                 console.log('catch', res)
             });
-         //    $scope.login.grant_type = 'password';
-         //    $scope.login.userType = 1;
-
-        	// let url = API.BaseUrl+'login';
-        	// QueryService.Login(url, $scope.login)
-        	// .then(function(res){
-        	// 	console.log(res);
-        	// }).catch(function(res){
-        	// 	console.log(res);
-        	// });
         }
 
     }
