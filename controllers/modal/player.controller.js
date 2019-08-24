6684b5431ffed8ce3aee60bbd0226be7bab68d9d
Ming <phieulangqn@gmail.com>
2019-08-25 00:02:29 +0700

    f

--- controllers/modal/player.controller.js
+++ controllers/modal/player.controller.js
@@ -30,7 +30,9 @@
                         "description": "",
                         // "sourceURL": 'http://' + link,//'https://3.15.208.194:1935/LRTV/dheerajLive11/playlist.m3u8',
                         "sources": [
-                            { "sourceURL": "rtsp://" + link }
+                            { "sourceURL": "https://" + link },
+                            { "sourceURL": "rtsp://" + link },
+                            { "sourceURL": "http://" + link },
                         ],
                         "autoPlay": true,
                         "volume": "75",
