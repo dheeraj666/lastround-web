f5185627f9b6aafc54d89c8089d81c448bb03959
Ming <phieulangqn@gmail.com>
2019-12-21 17:35:47 +0700

    'sign'

--- controllers/authApple.controller.js
+++ controllers/authApple.controller.js
@@ -10,18 +10,24 @@
         console.log($location)
         function init() {
             PreloadingService.loadStart()
-            $http.get(API.BaseUrl + 'auth-apple').then(function (res) {
-                if (res.data.status == 1) {
-                    AuthenService.setAuthen(res.data.data);
-                    toaster.pop('success', 'Wellcome back! ' + res.data.data.username);
-                }
-                location.href = '/#!/'
-                PreloadingService.loadEnd()
-            }).catch(function (res) {
-                if (res.data && res.data.msg)
-                    toaster.pop('error', res.data.msg)
-                PreloadingService.loadEnd()
-            });
+            if ($location.$$search.code) {
+                $http.post(API.BaseUrl + 'auth-apple', { code: $location.$$search.code }, {
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                }).then(function (res) {
+                    if (res.data.status == 1) {
+                        AuthenService.setAuthen(res.data.data);
+                        toaster.pop('success', 'Wellcome back! ' + res.data.data.username);
+                    }
+                    location.href = '/#!/'
+                    PreloadingService.loadEnd()
+                }).catch(function (res) {
+                    if (res.data && res.data.msg)
+                        toaster.pop('error', res.data.msg)
+                    PreloadingService.loadEnd()
+                });
+            }
         }
         init()
     }
